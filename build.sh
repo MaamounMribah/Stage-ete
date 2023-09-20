26d6e5f7474443639567774c6f05bc105d973dbc
maamoun <mribahmaamoun@gmail.com>
2023-09-20 23:32:27 +0100

    dep10

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 set -o errexit  # exit on error
-
+python3 -m venv dev-env
+source venv/bin/activate
 pip install -r requirements.txt
 
 python3 manage.py collectstatic --no-input
