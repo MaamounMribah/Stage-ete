18fe375fa14d2ec52b46b85ba3f9002a597bcd85
maamoun <mribahmaamoun@gmail.com>
2023-09-20 22:29:45 +0100

    dep5

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ set -o errexit  # exit on error
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+python3 manage.py collectstatic --no-input
+python3 manage.py makemigrations
+python3 manage.py migrate
