0f67d2e18c1ffe0a919d12a6485d7e4d5edb9c12
maamoun <mribahmaamoun@gmail.com>
2023-09-21 20:46:08 +0100

    dep14

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
 }
 """
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL')),
 }
 
 # Password validation
