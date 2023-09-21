cd3cf4ac80f6f5fd598ebf23f2ce7e0ebbadfc6f
maamoun <mribahmaamoun@gmail.com>
2023-09-21 20:11:56 +0100

    dep11

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 set -o errexit  # exit on error
-python3 -m venv venv && source venv/bin/activate
+python3 -m venv venv
+source venv/bin/activate
 pip install -r requirements.txt
 
-python3 manage.py collectstatic --no-input
+
 python3 manage.py makemigrations
 python3 manage.py migrate
