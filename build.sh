9e2f16687416472a3e25f221ad10bcdd4daf45c7
maamoun <mribahmaamoun@gmail.com>
2023-09-21 21:53:16 +0100

    dep15

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ pip install dj-database-url
 pip install python-dotenv
 pip install django-cors-headers
 pip install psycopg2 -U
+python3 -m pip install Pillow
 
 python3 -m venv venv
 source venv/bin/activate
