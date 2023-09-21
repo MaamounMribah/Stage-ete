940a06b2e88fb0868dcc5bc9ed8ef1053621aa09
maamoun <mribahmaamoun@gmail.com>
2023-09-21 21:25:19 +0100

    dep14

--- build.sh
+++ build.sh
@@ -11,4 +11,5 @@ python3 manage.py makemigrations
 
 python3 manage.py makemigrations store
 python3 manage.py migrate
+python3 manage.py collectstatic --no-input
 python manage.py createsuperuser --no-input
