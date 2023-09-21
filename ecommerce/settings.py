406e2113e5c573fd0a84bdc70f18e1fe71c637c0
maamoun <mribahmaamoun@gmail.com>
2023-09-21 22:15:42 +0100

    dep15

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -138,12 +138,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = './static/'
-#STATIC_ROOT = './static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-MEDIA_URL = '/images/'
+
+# MEDIA_URL = '/images/'
+MEDIA_URL = 'static/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
